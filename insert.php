d5ecc31ade2a2e1bdd94c25ec708b0380ce0043e
SGit0 <58560730+SGit0@users.noreply.github.com>
2019-12-18 09:57:36 +0100

    ajout variables $nom, $prenom

--- insert.php
+++ insert.php
@@ -4,6 +4,10 @@ $username = "user";
 $password = "password";
 $dbname = "stage";
 
+
+$Xnom = $_POST['prenom'];
+$Xprenom = $_POST['nom'];
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,7 +15,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$conn->query($sql = "INSERT INTO stagiaire (nom, prenom) VALUES (\"$_POST['nom']\", \"$_POST['prenom']\")");
+$conn->query($sql = "INSERT INTO stagiaire (nom, prenom) VALUES ('$Xnom', '$Xprenom')");
 
 
 $conn->close();
